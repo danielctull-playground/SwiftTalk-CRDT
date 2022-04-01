f9d4f31cb42c9424e43ff966913b445f24b9e918
Daniel Tull <dt@danieltull.co.uk>
2022-04-01 15:39:15 +0100

    Test idempotency and associativity

--- Tests/CRDTKitTests/MaxTests.swift
+++ Tests/CRDTKitTests/MaxTests.swift
@@ -4,11 +4,27 @@ import XCTest
 
 final class MaxTests: XCTestCase {
     
-    func test() {
+    func testCommutativity() {
         for _ in 0..<1000 {
             let a = Max(value: Int.random)
             let b = Max(value: Int.random)
             XCTAssertEqual(a.merging(b), b.merging(a))
+        }
+    }
+    
+    func testIdempotency() {
+        for _ in 0..<1000 {
+            let a = Max(value: Int.random)
+            XCTAssertEqual(a.merging(a), a)
+        }
+    }
+    
+    func testAssociativity() {
+        for _ in 0..<1000 {
+            let a = Max(value: Int.random)
+            let b = Max(value: Int.random)
+            let c = Max(value: Int.random)
+            XCTAssertEqual(a.merging(b).merging(c), a.merging(b.merging(c)))
         }
     }
 }
