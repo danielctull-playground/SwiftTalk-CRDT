aedb7abb1fc9b516d05bda609745c4bae11ded4f
Daniel Tull <dt@danieltull.co.uk>
2022-04-01 16:29:58 +0100

    Add behavioural test

--- Tests/CRDTKitTests/MaxTests.swift
+++ Tests/CRDTKitTests/MaxTests.swift
@@ -27,6 +27,19 @@ final class MaxTests: XCTestCase {
             XCTAssertEqual(a.merging(b).merging(c), a.merging(b.merging(c)))
         }
     }
+
+    func testBehavior() {
+        var a = Max(10)
+        var b = Max(0)
+        b.merge(a)
+        XCTAssertEqual(b.value, 10)
+        a.value += 1
+        b.value += 1
+        a.merge(b)
+        b.merge(a)
+        XCTAssertEqual(a.value, 11)
+        XCTAssertEqual(b.value, 11)
+    }
 }
 
 extension Int {
