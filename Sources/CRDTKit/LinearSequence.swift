afdc38ec33828f5bb7eda124370235fc28f036e5
Daniel Tull <dt@danieltull.co.uk>
2022-07-26 21:49:52 +0100

    Add CRDT conformance to LinearSequence

--- Sources/CRDTKit/LinearSequence.swift
+++ Sources/CRDTKit/LinearSequence.swift
@@ -7,6 +7,40 @@ public struct LinearSequence<Element> {
     public init(site: SiteID) {
         self.site = site
     }
+
+    public init() {
+        self.init(site: SiteID())
+    }
+}
+
+extension LinearSequence: CRDT {
+
+    public mutating func merge(_ other: LinearSequence<Element>) {
+        for node in other._elements {
+            _insert(node)
+        }
+    }
+
+    private mutating func _insert(_ node: Node) {
+        let index: Int
+        if let parent = node.parent {
+            index = _elements.firstIndex(where: { $0.id == parent })! + 1
+        } else {
+            index = 0
+        }
+        for existingIndex in _elements[index...].indices {
+            let existing = _elements[existingIndex]
+            if existing.id == node.id {
+                // TODO: Safety checks
+                return
+            }
+            if existing.id < node.id {
+                _elements.insert(node, at: existingIndex)
+                return
+            }
+        }
+        _elements.append(node)
+    }
 }
 
 extension LinearSequence {
@@ -15,7 +49,7 @@ extension LinearSequence {
         let parent = index > 0 ? _elements[index - 1].id : nil
         let id = NodeID(site: site, time: clock)
         let node = Node(parent: parent, id: id, value: value)
-        _elements.insert(node, at: index)
+        _insert(node)
         clock += 1
     }
 
--- Tests/CRDTKitTests/LinearSequenceTests.swift
+++ Tests/CRDTKitTests/LinearSequenceTests.swift
@@ -17,4 +17,46 @@ final class LinearSequenceTests: XCTestCase {
             }
         }
     }
+
+    func testMerge() {
+
+        var siteA = LinearSequence<Character>(site: "SiteA")
+        siteA.insert("a", at: 0)
+        siteA.insert("b", at: 1)
+
+        var siteB = LinearSequence<Character>(site: "SiteB")
+        siteB.merge(siteA)
+
+        XCTAssertEqual(siteA.elements, siteB.elements)
+
+        siteA.insert("c", at: 2)
+        siteA.insert("d", at: 3)
+
+        siteB.insert("e", at: 2)
+        siteB.insert("f", at: 3)
+
+        siteA.merge(siteB)
+        siteB.merge(siteA)
+
+        XCTAssertEqual(siteA.elements, siteB.elements)
+        XCTAssertEqual(String(siteA.elements), "abcdef")
+    }
+
+    func testLaws() {
+        LinearSequence<Int>.testCommutativity(equating: \.elements)
+        LinearSequence<Int>.testAssociativity(equating: \.elements)
+        LinearSequence<Int>.testIdempotency(equating: \.elements)
+    }
+}
+
+extension LinearSequence: Randomizable where Element == Int {
+
+    static var random: LinearSequence<Int> {
+        let count = Int.random(in: 1...10)
+        var sequence = LinearSequence()
+        for index in 0...count {
+            sequence.insert(Int.random, at: index)
+        }
+        return sequence
+    }
 }
